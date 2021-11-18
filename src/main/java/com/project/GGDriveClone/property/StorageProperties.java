e7f6681abd094f21829965b854181bac51e57c0b
Luu Thanh Dat <datlt132@gmail.com>
2021-11-18 23:52:28 +0700

    fix add file

---
+++ src/main/java/com/project/GGDriveClone/property/StorageProperties.java
@@ -0,0 +1,21 @@
+package com.project.GGDriveClone.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "/resources/upload";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
--- src/main/java/com/project/GGDriveClone/service/FileService.java
+++
@@ -1,4 +0,0 @@
-package com.project.GGDriveClone.service;
-
-public class FileService {
-}
