d993e422a390eaf2d2065dc8087dc093ec3f3b16
DAI014 <dai.nv014@gmail.com>
2021-11-19 20:01:36 +0700

    add user management

--- src/main/java/com/project/GGDriveClone/service/UserService.java
+++ src/main/java/com/project/GGDriveClone/service/UserService.java
@@ -1,4 +1,16 @@
 package com.project.GGDriveClone.service;
 
-public class UserService {
+import com.project.GGDriveClone.entity.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<UserEntity> getAllUser();
+
+    void saveUser(UserEntity user);
+
+    void deleteUser(Long id);
+
+    Optional<UserEntity> findUserById(Long id);
 }
