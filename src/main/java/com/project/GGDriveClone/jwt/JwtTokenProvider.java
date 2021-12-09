02b9352c779365f42601f24e95a9256007f2da56
datlt <khangbt@rabiloo.com.vn>
2021-12-09 15:30:08 +0700

    add jwt, change form login -> api login

---
+++ src/main/java/com/project/GGDriveClone/jwt/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.project.GGDriveClone.jwt;
+
+import com.project.GGDriveClone.security.CustomUserDetails;
+import io.jsonwebtoken.*;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private org.slf4j.Logger Logger = LoggerFactory.getLogger(JwtTokenProvider.class);
+    // Đoạn JWT_SECRET này là bí mật, chỉ có phía server biết
+    private final String JWT_SECRET = "lodaaaaaa";
+
+    //Thời gian có hiệu lực của chuỗi jwt
+    private final long JWT_EXPIRATION = 604800000L;
+
+    // Tạo ra jwt từ thông tin user
+    public String generateToken(CustomUserDetails userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
+        // Tạo chuỗi json web token từ id của user.
+        return Jwts.builder()
+                .setSubject(Long.toString(userDetails.getUserId()))
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+    // Lấy thông tin user từ jwt
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException ex) {
+            Logger.info("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            Logger.info("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            Logger.info("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            Logger.info("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
--- src/main/java/com/project/GGDriveClone/security/CustomUserDetails.java
+++ src/main/java/com/project/GGDriveClone/security/CustomUserDetails.java
@@ -47,4 +47,24 @@ public class CustomUserDetails extends User {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
