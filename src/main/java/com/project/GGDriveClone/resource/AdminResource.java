1706c909c68015356724e1facdd426c27fb52836
DAI014 <dai.nv014@gmail.com>
2021-12-14 13:30:29 +0700

    clean code

--- src/main/java/com/project/GGDriveClone/resource/AdminResource.java
+++ src/main/java/com/project/GGDriveClone/resource/AdminResource.java
@@ -1,15 +1,14 @@
 package com.project.GGDriveClone.resource;
 
-        import com.project.GGDriveClone.entity.UserEntity;
-        import com.project.GGDriveClone.repository.UserRepository;
-        import com.project.GGDriveClone.service.UserService;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.*;
+import com.project.GGDriveClone.entity.UserEntity;
+import com.project.GGDriveClone.repository.UserRepository;
+import com.project.GGDriveClone.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-        import javax.validation.Valid;
-        import java.util.List;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -32,25 +31,11 @@ public class AdminResource {
     }
 
 
-//    @PostMapping(value = "/add")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public ResponseEntity<UserEntity> addUser(@RequestParam(value = "name") String name, @RequestParam(value = "email")
-//            String email, @RequestParam(value = "password") String password) {
-//        UserEntity user = new UserEntity();
-//        user.setEmail(name);
-//        user.setEmail(password);
-//        user.setEmail(email);
-//        userService.saveUser(user);
-//        return ResponseEntity.ok().body(user);
-//    }
-
-
     @PostMapping("/add")
     public ResponseEntity<UserEntity> createUser(@Valid @RequestBody UserEntity user) {
         userService.saveUser(user);
         return ResponseEntity.ok(user);
     }
-
 
 
     @PutMapping("/{id}")
@@ -65,24 +50,4 @@ public class AdminResource {
         return ResponseEntity.ok(user);
     }
 
-//    @PutMapping("/{id}")
-//    public UserEntity update_user(@PathVariable("id") Long id, @Valid @RequestBody UserEntity user) {
-//        user.setId(id);
-//        userService.saveUser(user);
-//    }
-//
-
-
-//    @RequestMapping(value = "/save", method  = RequestMethod.POST)
-//    public String save(UserEntity user) {
-//
-//        userService.saveUser(user);
-//        return "redirect:/";
-//    }
-//
-//    @RequestMapping(value = "/delete", method = RequestMethod.GET)
-//    public String deleteUser(@RequestBody Long userId, Model model) {
-//        userService.deleteUser(userId);
-//        return "redirect:/";
-//    }
 }
--- src/main/java/com/project/GGDriveClone/resource/UserResource.java
+++ src/main/java/com/project/GGDriveClone/resource/UserResource.java
@@ -26,36 +26,13 @@ public class UserResource {
     public ResponseEntity<UserEntity> register(@Valid @RequestBody UserEntity user, HttpServletRequest request)
             throws UnsupportedEncodingException, MessagingException {
        service.register(user,  getSiteURL(request));
-       System.out.println("abccc");
        return ResponseEntity.ok(user);
     }
 
-//    @PostMapping("/process_register")
-//    public String processRegister(UserEntity user, HttpServletRequest request)
-//            throws UnsupportedEncodingException, MessagingException {
-//        service.register(user, getSiteURL(request));
-//        return "register_success";
-//    }
-
-//    @GetMapping("/users")
-//    public String listUsers(Model model) {
-//        List<UserEntity> listUsers = service.listAll();
-//        model.addAttribute("listUsers", listUsers);
-//
-//        return "users";
-//    }
 
     private String getSiteURL(HttpServletRequest request) {
         String siteURL = request.getRequestURL().toString();
         return siteURL.replace(request.getServletPath(), "");
     }
 
-    @GetMapping("/verify")
-    public String verifyUser(@Param("code") String code) {
-        if (service.verify(code)) {
-            return "verify_success";
-        } else {
-            return "verify_fail";
-        }
-    }
 }
